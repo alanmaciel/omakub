24107a6ffa9edbe4be36ba6c854e330653bb298a
Alan Maciel <alan.maciel.salcedo@gmail.com>
2024-12-30 21:48:48 -0600

    Cursor ai-powered ide

--- install/desktop/optional/app-cursor.sh
+++ install/desktop/optional/app-cursor.sh
@@ -2,17 +2,19 @@ cd /tmp
 curl -L o cursor.appimage https://downloader.cursor.sh/linux/appImage/x64
 sudo mv cursor.appimage /opt/cursor.appimage
 sudo chmod +x /opt/cursor.appimage
-sudo apt install -y fuse
+sudo apt install -y fuse3
+sudo apt install libfuse2t64
 
 DESKTOP_FILE="/usr/share/applications/cursor.desktop"
 
 sudo bash -c "cat > $DESKTOP_FILE" <<EOL
 [Desktop Entry]
 Name=Cursor
+Comment=AI-powered code editor
 Exec=/opt/cursor.appimage --no-sandbox
-Icon=/opt/cursor.png
+Icon=/home/$USER/.local/share/omakub/applications/icons/cursor.png
 Type=Application
-Categories=Development;
+Categories=Development;IDE;
 EOL
 
 if [ -f "$DESKTOP_FILE" ]; then
